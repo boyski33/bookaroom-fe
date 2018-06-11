b406937521cb2842aab4f04024cc98cf35b777b4
Boyan Kushlev <takarabukara@gmail.com>
2018-06-11 21:29:14 +0300

    MVP ready

--- src/app/room-card/room-card.component.ts
+++ src/app/room-card/room-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Room } from '../room';
 import { RoomsService } from '../rooms.service';
 import * as _ from 'lodash';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-room-card',
@@ -9,11 +10,15 @@ import * as _ from 'lodash';
   styleUrls: ['./room-card.component.css']
 })
 export class RoomCardComponent implements OnInit {
+  buttonText = 'Book';
   @Input() room: Room;
 
   constructor(private _rs: RoomsService) { }
 
   ngOnInit() {
+    if (this.room.isBooked) {
+      this.buttonText = 'Booked';
+    }
   }
 
   bookRoom() {
@@ -21,6 +26,20 @@ export class RoomCardComponent implements OnInit {
     tempRoom.isBooked = true;
 
     this._rs.updateRoom(tempRoom)
-      .subscribe(() => this.room.isBooked = true);
+      .subscribe(() => {
+        this.room.isBooked = true;
+        this.buttonText = 'Booked';
+      });
+  }
+
+  cancelRoom() {
+    const tempRoom = _.cloneDeep(this.room);
+    tempRoom.isBooked = false;
+
+    this._rs.updateRoom(tempRoom)
+      .subscribe(() => {
+        this.room.isBooked = false;
+        this.buttonText = 'Book';
+      });
   }
 }
